63abe1596e033ead867a18be13f45c1c1f672fdb
1621-nicolas <146853228+1621-nicolas@users.noreply.github.com>
2026-09-10 22:02:32 -0500

    feat: add routes for SignaText modes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { InicioComponent } from './pages/inicio/inicio';
+import { SalaComponent } from './pages/sala/sala';
+import { LlamadaComponent } from './pages/llamada/llamada';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'inicio'
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+    title: 'SignaText | Inicio'
+  },
+  {
+    path: 'traductor',
+    component: App,
+    title: 'SignaText | Traductor local'
+  },
+  {
+    path: 'sala',
+    component: SalaComponent,
+    title: 'SignaText | Sala privada'
+  },
+  {
+    path: 'llamada',
+    component: LlamadaComponent,
+    title: 'SignaText | Videollamada'
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio'
+  }
+];
