7cf2754ce9be0b2168a1ba175046827f60637198
1621-nicolas <146853228+1621-nicolas@users.noreply.github.com>
2026-09-10 22:02:07 -0500

    feat: add video call page

---
+++ src/app/pages/llamada/llamada.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-llamada',
+  standalone: true,
+  templateUrl: './llamada.html',
+  styleUrl: './llamada.css'
+})
+export class LlamadaComponent {}
